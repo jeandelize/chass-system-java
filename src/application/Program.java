7de0966d8d7da9709a2a114ecdef17dc366fdc2e
Jean Delize <jeanmarinodelize@gmail.com>
2021-05-29 10:11:04 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Chess project");
+
+      Position pos = new Position(3, 5);
+      
+      System.out.println(pos);
+		
 	}
 
 }
